3ba9811fd1a58de0e845eac378e4eca6c7110b2b
Kasper Johansen <kaspernj@gmail.com>
2013-09-14 16:25:59 +0200

    Moved source into dk.vimk.meyer namespace. Solved encoding bugs in source.

--- src/main/java/dk/vimk/meyer/Meyer.java
+++ src/main/java/dk/vimk/meyer/Meyer.java
@@ -1,11 +1,11 @@
 // opgave 3 fra klasser og kontrolstrukturer fortsat - opbygning af spillet meyer med forenklede regler 
 
-package meyer;
+package dk.vimk.meyer;
 /**
  
  * @author Kim Vammen
  */
-public class Main
+public class Meyer
 {  
     private static Player computer;
     private static Player menneske;
